d773c6bff5a240115c95af56b735238d4f07bc7d
mahmoud_housam <mahmoudhousam60@gmail.com>
2020-12-16 20:22:56 +0200

    explore_nans chart edits

--- helpers.py
+++ helpers.py
@@ -22,24 +22,41 @@ def explore_nans(df, title):
     not_null_values = df_not_null.values.tolist()
     data = [
         go.Bar(
-            name="Null",
-            x=null_values,
-            y=labels,
-            orientation="h",
-            marker=dict(
-                color="rgb(0,0,128)",
-            ),
-        ),
-        go.Bar(
             name="Not Null",
             x=not_null_values,
             y=labels,
             orientation="h",
             marker=dict(
-                color="rgb(0,128,0)",
+                color="rgb(128,128,128)",
+            ),
+        ),
+        go.Bar(
+            name="Null",
+            x=null_values,
+            y=labels,
+            orientation="h",
+            marker=dict(
+                color="rgb(192,192,192)",
             ),
         ),
     ]
     layout = go.Layout(title=title, barmode="stack", width=1200, height=500)
     fig = go.Figure(data, layout)
     return fig.show()
+
+
+def drop_60_missings(df):
+    """a function that takes a
+    dataframe and return new dataframe with columns dropped that have more
+    that 60% null values and so on we cannot take any decision about.
+
+    Args:
+        df: pandas dataframe
+
+    Returns:
+        df: new dataframe
+    """
+    for col in df.columns:
+        if df[col].isnull().sum() / len(df) * 100 >= 60:
+            df.drop(columns=[col], inplace=True)
+    return df
